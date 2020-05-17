6e966810a9355787e45cc190697a8c548664cf31
alonyomtov123 <53430928+alonyomtov123@users.noreply.github.com>
2020-05-17 14:40:28 +0300

    Update sqlGui.py

--- sqlGui.py
+++ sqlGui.py
@@ -2,28 +2,20 @@ import PySimpleGUI as sg
 import sqlite3
 import os
 from shutil import copyfile
-import sounddevice as sd
-from scipy.io.wavfile import write
 
 def getUserData(dbPath):
-    sg.theme('DarkAmber') #color
-    fs = 44100
-    seconds = 5
-
-    #window layout
+    sg.theme('DarkAmber')	# Add a touch of color
+    # All the stuff inside your window.
     layout = [  [sg.Text('')],
                 [sg.Text('Enter Username: '), sg.InputText()],
                 [sg.Text('Enter Password:  '), sg.InputText()],
-                [sg.ReadButton('Record')],
                 [sg.Button('Ok')] ]
 
     # Create the Window
     window = sg.Window('Sign Up', layout)
-    
     # Event Loop to process "events" and get the "values" of the inputs
     while True:
         event, values = window.read()
-        #values are inputs from user
         if("@" not in values[0] or "." not in values[0] or len(values[0]) == 0 or len(values[1]) == 0):
             sg.Popup('Username is invalid')
             continue
@@ -32,36 +24,25 @@ def getUserData(dbPath):
             c = conn.cursor()
             c.execute("SELECT COUNT(*) FROM User")
             number_of_entries = c.fetchone()[0]
-
-            #check if username is not used.
             if number_of_entries != 0:
                 c = conn.cursor()
                 c.execute("SELECT userId FROM User WHERE userId=" + values[0])
                 if not c.fetchall():
                     sg.Popup('Username is already used')
                 else:
-                    #add user
                     c = conn.cursor()
                     c.execute("INSERT INTO User VALUES (?, ?, ?, ?)", (number_of_entries + 1, values[0], values[1], 0))
                     conn.commit()
                     break
             else:
-                #add user
                 c = conn.cursor()
                 c.execute("INSERT INTO User VALUES (?, ?, ?, ?)", (number_of_entries + 1, values[0], values[1], 0))
                 conn.commit()
                 break
             conn.close()
-
-            if event == 'Record':
-                sg.Popup("You have 5 seconds. Say your name and where you live")
-                myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
-                sd.wait()  # Wait until recording is finished
-                dest = "/opt/mycroft/skills/useridentification-skill/allUsers/" + str(number_of_entries + 1) + "-" + values[0] + "-1" + ".wav"
-                write(dest, fs, myrecording)
     window.close()
-    #source = "/tmp/mycroft_utterances/" + getCurrentUserAnswer()
-    #copyfile(source, dest)
+    dest = "/opt/mycroft/skills/useridentification-skill/allUsers/" + str(number_of_entries + 1) + "-" + values[0] + "-1" + ".wav"
+    copyfile(getCurrentUserAnswer(), dest)
 
 def getCurrentUserAnswer():
 	#get current question sound file	
